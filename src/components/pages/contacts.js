c0a8818d6cab9210d3718038a49128b61618784e
Alessandro <alessandrodalgobbo@gmail.com>
2018-04-25 13:11:08 +0200

    Restyling of CV component, created container-table CSS class

--- src/components/pages/contacts.js
+++ src/components/pages/contacts.js
@@ -1,67 +1,69 @@
 import React, { Component } from 'react';
-
+import cv_pdf from '../../assets/images/CV_Alessandro_Dal_Gobbo.pdf';
 
 class Contacts extends Component {
   render() {
     return (
       <div className="container-fixed">
+        <div className="container-fixed div-box-shadow m-b-l">
         <div className="container one-r full-c-third">
           <div className="one-r container">
-            <h3>Personal info</h3>
+            <h3>Personal info</h3> 
             </div>
         </div>    
-      <div className="container two-r full-c-third">
-        <div className="one-r full-c-third container">
-          <div className="f-c-third">
-              Name
+        <div className="container two-r full-c-third">
+          <div className="one-r full-c-third container">
+            <div className="f-c-third">
+                Name
+            </div>
+            <div className="st-c-third">
+              Alessandro Dal Gobbo
+            </div>
           </div>
-          <div className="st-c-third">
-            Alessandro Dal Gobbo
+          <div className="two-r full-c-third container">
+            <div className="f-c-third">
+                Address
+            </div>
+            <div className="st-c-third">
+              Via Manci 19, Bolzano (BZ)
+            </div>
           </div>
-        </div>
-        <div className="two-r full-c-third container">
-          <div className="f-c-third">
-              Address
+          <div className="three-r full-c-third container">
+            <div className="f-c-third">
+              Phone
+            </div>
+            <div className="st-c-third">
+              3333321008  
+            </div>
           </div>
-          <div className="st-c-third">
-            Via Manci 19, Bolzano (BZ)
+          <div className="four-r full-c-third container">
+            <div className="f-c-third">
+              Email
+            </div>
+            <div className="st-c-third">
+              alessandrodalgobbo@gmail.com
+            </div>
           </div>
-        </div>
-        <div className="three-r full-c-third container">
-          <div className="f-c-third">
-            Phone
+          <div className="five-r full-c-third container">
+            <div className="f-c-third">
+              Nationality
+            </div>
+            <div className="st-c-third">
+            Italy
+            </div>
           </div>
-          <div className="st-c-third">
-            3333321008  
-          </div>
-        </div>
-        <div className="four-r full-c-third container">
-          <div className="f-c-third">
-            Email
-          </div>
-          <div className="st-c-third">
-            alessandrodalgobbo@gmail.com
-          </div>
-        </div>
-        <div className="five-r full-c-third container">
-          <div className="f-c-third">
-            Nationality
-          </div>
-          <div className="st-c-third">
-           Italy
-          </div>
-        </div>
-        <div className="six-r full-c-third container">
-          <div className="f-c-third">
-            Date of birth
-          </div>
-          <div className="st-c-third">
-            10/06/1993
+          <div className="six-r full-c-third container">
+            <div className="f-c-third">
+              Date of birth
+            </div>
+            <div className="st-c-third">
+              10/06/1993
+            </div>
           </div>
         </div>
       </div>
-      <div className="container-fixed three-r full-c-third">
-        <div className="one-r">
+      <div className="container-fixed three-r full-c-third div-box-shadow m-b-l">
+        <div className="one-r container">
           <h3>Work experience</h3>
         </div>
         <div className="two-r container-fixed m-b-m">
@@ -97,7 +99,127 @@ class Contacts extends Component {
             Design, implementation and depoyment of a website for the StartUp Dubi, consisting of a platform for research and development
           </div>
         </div>
-      </div> 
+      </div>
+      <div className="container-fixed four-r full-c-third div-box-shadow m-b-l">
+        <div className="one-r container">
+          <h3>Education</h3>
+        </div>
+        <div className="container-fixed">
+        <div className="full-c-third m">
+          10/2015 - today: Bachelor in Computer Science and Enginnering at Free University of Bolzano
+          </div>
+      </div>
+
+        <div className="container-fixed">
+        <div className="full-c-third">
+          09/2017 - 07/2012: Scientific high school "E.Torricelli" at Bolzano 
+          </div>
+      </div>
+
+        <div className="container-fixed">
+        <div className="full-c-third">
+          11/07/2011 - 29/07/2011: German course with certification of level C1 at "inlingua Sprachcenter Berlin - The inlingua School of Languages"
+          </div>
+       </div>
+
+       <div className="container-fixed">
+         <div className="full-c-third">
+          03/07/2010 - 16/07/2010: English course with oral, written and listening exam of level "Upper Intermediate" at "Atlantic Language Galway" 
+          </div>
+      </div>
+      </div>
+      <div className="container-fixed five-r full-c-third m-b-l div-box-shadow">
+        <div className="one-r container-fixed">
+          <h3>Skills</h3>
+        </div>
+        <div className="container-fixed">
+          <div className="full-c-third container one-r">
+            <div className="f-c-third one-r">
+              Languages
+            </div>
+          </div>
+          <div className="st-c-third container-table two-r">
+          <div className="f-c-third one-r">
+              Native
+            </div>
+            <div className="st-c-third one-r">
+              Italian
+            </div>
+            <div className="st-c-third two-r container-small m-b-m m-t-m">
+              <div className="f-c-third">
+                Reading
+              </div>
+              <div className="s-c-third">
+                Writing
+              </div>
+              <div className="t-c-third">
+                Speaking              
+                </div>
+              </div> 
+              <div className="f-c-third three-r container-small">
+                English
+              </div>
+              <div className="st-c-third three-r container-small">
+                <div className="f-c-third">
+                  Excellent
+                </div>
+                <div className="s-c-third">
+                  Excellent
+                </div>
+                <div className="t-c-third">
+                  Excellent
+                </div> 
+              </div>
+              <div className="f-c-third four-r container-small">
+                German
+              </div>
+              <div className="st-c-third four-r container-small">
+                <div className="f-c-third">
+                  Excellent
+                </div>
+                <div className="s-c-third">
+                  Excellent
+                </div>
+                <div className="t-c-third">
+                  Good
+                </div> 
+              </div>
+          </div>
+        </div>
+        <div className="container-fixed m-t-l">
+          <div className="full-c-third container one-r">
+            <div className="f-c-third one-r">
+              Technical competences
+            </div>
+          </div>
+          <div className="row justify-text">
+            Good knowledge of programming languages Java, JavaScript, nodejs and of web development technologies CSS, HTML and SASS <br />
+            Basic knowledge in C, Ruby, Prolog and Haskell <br />
+            Experience with Amazon Web Services (Elastic beanstalk, S3, RDS, ACM), Expressjs (Backend framework for nodejs), TypeScript and ORM <br />
+            Good knowledge in designing and implementing relational databases and of SQL <br />
+            Basic knowledge in Arduino, obtained during a one week Workshop at the Free University of Bolzano <br />
+            Basic knowledge of eletronics, of the Linux OS and of the Android development environment "Android Studio"
+          </div>
+        </div>
+        <div className="container-fixed m-t-l">
+          <div className="full-c-third container one-r">
+            <div className="f-c-third one-r">
+              Hobbies
+            </div>
+          </div>
+          <div className="row justify-text">
+            Other then Computer Science stuff I enjoy doing sports, as italian I obviously love football, but on my freetime I like go climbing and trecking
+          </div>
+        </div>
+      </div>
+      <div className="full-c-third container">
+        <div className="fs-c-third one-r">
+          If you liked my hystory you can download here my complete Curriculum Vitae in PDF
+        </div>
+        <div className="one-r">
+        <a href={cv_pdf}>CV_Alessandro_Dal_Gobbo.pdf</a>
+        </div>
+      </div>
     </div>
     );
   }
